8c6692cdfd8f9286b12ee7853734f098919f07dc
Oscar Morrison <me@oscarmorrison.com>
2015-10-16 22:32:53 -0500

    Updated username

--- content/js/functions.js
+++ content/js/functions.js
@@ -1,10 +1,6 @@
 var eachEntryJson = [];
 var pageNum = 1;
 var myLayer;
-var usernames = {
-    "mapBoxToken": "pk.eyJ1Ijoib3NjYXJtb3JyaXNvbiIsImEiOiI2MzAzMjZhNjEwMDlmZmZkMTExZWRjMzFlMTFhN2Q3OSJ9.YIbnXYEGI6911AjBV_0EBw",
-    "tumblrPage": "http://tahneeandoscar.tumblr.com"
-}
 
 
 var url = getUrl();
--- content/js/usernames.js
+++ content/js/usernames.js
@@ -1,4 +1,4 @@
 var usernames = {
-	"mapBoxToken":"pk.eyJ1Ijoib3NjYXJtb3JyaXNvbiIsImEiOiI2MzAzMjZhNjEwMDlmZmZkMTExZWRjMzFlMTFhN2Q3OSJ9.YIbnXYEGI6911AjBV_0EBw",
-	"tumblrPage":"http://tahneeandoscar.tumblr.com"
-	}
+  "mapBoxToken": "pk.eyJ1Ijoib3NjYXJtb3JyaXNvbiIsImEiOiI2MzAzMjZhNjEwMDlmZmZkMTExZWRjMzFlMTFhN2Q3OSJ9.YIbnXYEGI6911AjBV_0EBw",
+	"tumblrPage": "http://blog.tahneeandoscar.com"
+}
